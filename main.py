a1c61a213afa039b6e3c559eba132aee9d01a888
Димон <nodilka1@gmail.com>
2024-02-15 16:51:57 +0300

    Add tests

--- main.py
+++ main.py
@@ -1,21 +1,6 @@
-import unittest
-
-
-class TestAddFunction(unittest.TestCase):
-    def test_add_integers(self):
-        self.assertEqual(add(1, 2), 3)
-
-    def test_add_floats(self):
-        self.assertEqual(add(1.1, 2.2), 3.3)
-
-    def test_add_strings(self):
-        self.assertEqual(add("hello ", "world"), "hello world")
-
-
 def add(a, b):
     return a + b
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    unittest.main()
+    add(2, 3)
--- tests.py
+++ tests.py
@@ -1,5 +1,11 @@
-import unittest
+import pytest
 
 from main import add
 
 
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+    assert add(-1, -1) == -2
+    assert add(0, 0) == 0
+    assert add(1.5, 2.5) == 4  # Тестирование с дробными числами
